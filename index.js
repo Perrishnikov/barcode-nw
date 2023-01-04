7d72381cc0b4aa81d54525cc4aca54b507ccee8c
perrishnikov <perrishnikov@gmail.com>
2023-01-03 21:10:23 -0600

    commit!

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const parsedUrl = new URL(location.href);
-console.log(parsedUrl.searchParams.get("partcode")); // "123"
+console.log(parsedUrl.searchParams.get("upc"));
 
-JsBarcode("#barcode", "12345");
+JsBarcode("#barcode", "033674156858", {
+  format: 'UPC',
+  width: 3,
+  margin: 24,
+  // font: "fantasy"
+});
